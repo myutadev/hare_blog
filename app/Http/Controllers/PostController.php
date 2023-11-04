642b6cf69612ae1cfe7e8c74133c4288a6d8eb69
myuta <myuta.dev@gmail.com>
2023-11-04 18:09:01 +0900

    dev pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,7 +16,14 @@ class PostController extends Controller
      */
     public function index()
     {
-        return view('posts.index');
+        // $posts = Post::all();
+        // $posts = Post::orderby('created_at', 'desc')->get();
+        // $posts = Post::latest()->get();
+        $posts = Post::latest()->paginate(4);
+
+
+
+        return view('posts.index', compact('posts'));
     }
 
     /**
